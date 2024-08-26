41a45231c1f809a16d6b73a47795dc0c14e6d217
Anmol Munnolli <anmolmunnolli@gmail.com>
2024-08-26 01:03:41 -0400

    snowflake ingestion

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -2,9 +2,20 @@ from dotenv import load_dotenv
 import os
 import snowflake.connector
 import logging
-logging.basicConfig(level=logging.DEBUG)
+import pandas
+from pyspark.sql import SparkSession
+
+# logging.basicConfig(level=logging.DEBUG)
 
 load_dotenv()
+
+spark = SparkSession.builder \
+    .appName("CSV to Snowflake") \
+    .config("spark.jars.packages", "net.snowflake:spark-snowflake_2.12:2.9.0-spark_3.1,net.snowflake:snowflake-jdbc:3.18.0") \
+    .getOrCreate()
+
+# Read CSV file into DataFrame
+df = spark.read.csv(r"C:\Users\anmol\Desktop\de\data\E_commerce_Website_Logs.csv", header=True, inferSchema=True)
 
 snow_flk_acc = os.getenv("SNOWFLAKE_ACCOUNT")
 snow_flk_username = os.getenv("SNOWFLAKE_USER")
@@ -34,6 +45,61 @@ cursor.execute(f"""
     AUTO_RESUME = TRUE;
 """)
 print("Warehouse created")
+
+create_table_query = '''
+        CREATE TABLE ecommerce_logs (
+            accessed_date TIMESTAMP,
+            duration_secs INTEGER,
+            network_protocol VARCHAR,
+            ip VARCHAR,
+            bytes INTEGER,
+            accessed_from VARCHAR,
+            age VARCHAR,
+            gender VARCHAR,
+            country VARCHAR,
+            membership VARCHAR,
+            language VARCHAR,
+            sales FLOAT,
+            returned VARCHAR,
+            returned_amount FLOAT,
+            pay_method VARCHAR
+        );
+            '''
+
+conn.cursor().execute(create_table_query)
+
+put_query = "PUT file:///C:/Users/anmol/Desktop/de/data/E_commerce_Website_Logs.csv @%ecommerce_logs"
+conn.cursor().execute(put_query)
+print("File uploaded to Snowflake stage")
+
+# COPY INTO command to load data into the table
+copy_query = """
+COPY INTO ecommerce_logs
+FROM @%ecommerce_logs/E_commerce_Website_Logs.csv
+FILE_FORMAT = (TYPE = 'csv', FIELD_OPTIONALLY_ENCLOSED_BY='"');
+"""
+conn.cursor().execute(copy_query)
+print("Data copied into ecommerce_logs table")
+
+
+options = {
+    "sfURL": f"https://{snow_flk_acc}.snowflakecomputing.com",
+    "sfUser": snow_flk_username,
+    "sfPassword": snow_flk_pass,
+    "sfDatabase": snow_flk_db,
+    "sfSchema": snow_flk_schema,
+    "sfWarehouse": snow_flk_warehouse
+}
+
+# Write DataFrame to Snowflake
+df.write.format("snowflake") \
+    .options(**options) \
+    .option("dbtable", "ecommerce_logs") \
+    .mode("overwrite") \
+    .save()
+
+# Cleanup
+spark.stop()
 
 cursor.close()
 conn.close()
