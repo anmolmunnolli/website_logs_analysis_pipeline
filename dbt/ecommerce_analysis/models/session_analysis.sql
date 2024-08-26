41ab076eb40b1cc1a97300b747d83d14ed64b58b
Anmol Munnolli <anmolmunnolli@gmail.com>
2024-08-26 15:30:48 -0400

    updating scripts

---
+++ dbt/ecommerce_analysis/models/session_analysis.sql
@@ -0,0 +1,20 @@
+WITH sessions AS (
+    SELECT
+        ip,
+        SUM(duration_(secs)) AS total_session_duration,
+        COUNT(*) AS sessions_count,
+        SUM(sales) AS total_sales,
+        SUM(returned_amount) AS total_returned,
+        MAX(network_protocol) AS most_common_protocol
+    FROM {{ source('ecommerce_source', 'ecommerce_logs') }}
+    GROUP BY ip
+)
+SELECT
+    ip,
+    total_session_duration,
+    sessions_count,
+    total_sales,
+    total_returned,
+    most_common_protocol
+FROM sessions
+LIMIT 5;
